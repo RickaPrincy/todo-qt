116403df62c0dd56f09de93dfd97983e4d5510ce
RickaPrincy <rckprincy@gmail.com>
2023-12-30 18:55:26 +0300

    feat: replace description to deadline

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -3,6 +3,7 @@
 #include <QDebug>
 #include <QStringList>
 #include <QMessageBox>
+#include <QPushButton>
 #include <QComboBox>
 
 MainWindow::MainWindow(QWidget *parent)
@@ -10,17 +11,19 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     setFixedSize(770, 400);
-    setWindowTitle("Ricka's todo");
+    setWindowTitle("Todo");
     ui->setupUi(this);
-    ui->todosTables->setColumnCount(3);
+    ui->todosTables->setColumnCount(4);
     QStringList todos_headers;
-    todos_headers << "Name" << "Description" << "Status";
+    todos_headers << "Name" << "Deadline" << "Status" << "Actions";
     ui->todosTables->setHorizontalHeaderLabels(todos_headers);
 
-    ui->todosTables->setColumnWidth(0, ui->todosTables->width() / 4);
-    ui->todosTables->setColumnWidth(1, ui->todosTables->width() / 2);
-    ui->todosTables->setColumnWidth(2, ui->todosTables->width() / 4);
+    ui->todosTables->setColumnWidth(0, 1.5 * ui->todosTables->width() / 5);
+    ui->todosTables->setColumnWidth(1, 1.5 * ui->todosTables->width() / 5);
+    ui->todosTables->setColumnWidth(2, ui->todosTables->width() / 5);
+    ui->todosTables->setColumnWidth(3, ui->todosTables->width() / 5);
     ui->todosTables->verticalHeader()->setVisible(false);
+    ui->deadline->setDateTime(QDateTime::currentDateTime());
 }
 
 MainWindow::~MainWindow()
@@ -31,22 +34,24 @@ MainWindow::~MainWindow()
 void MainWindow::on_addButton_clicked()
 {
     QString name = ui->inputName->text();
-    QString description = ui->inputDescription->text();
+    QDateTime deadline = ui->deadline->dateTime();
     ui->inputName->clear();
-    ui->inputDescription->clear();
+    ui->deadline->setDateTime(QDateTime::currentDateTime());
 
-    if(name.isEmpty() || description.isEmpty()){
-        QMessageBox::information(this, "Empty field", "Name and Description cannot be empty");
+    if(name.isEmpty() || !deadline.isValid()){
+        QMessageBox::information(this, "Empty field", "Name and Deadline cannot be empty");
         return;
     }
+
     int rowCount = ui->todosTables->rowCount();
 
     ui->todosTables->insertRow(rowCount);
     ui->todosTables->setItem(rowCount, 0, new QTableWidgetItem(name));
-    ui->todosTables->setItem(rowCount, 1, new QTableWidgetItem(description));
-    ui->todosTables->setItem(rowCount, 2, new QTableWidgetItem("false"));
 
-    // Create a QComboBox for the "Status" column
+    QDateTimeEdit *deadlineItem = new QDateTimeEdit();
+    deadlineItem->setDateTime(deadline);
+    ui->todosTables->setCellWidget(rowCount, 1, deadlineItem);
+
     QComboBox *status = new QComboBox();
 
     status->addItem("Pending");
@@ -57,4 +62,7 @@ void MainWindow::on_addButton_clicked()
     ui->todosTables->setCellWidget(rowCount, 2, status);
     status->setStyleSheet("QComboBox QAbstractItemView { selection-color: black; }");
 
+    QPushButton *deleteButton = new QPushButton();
+    deleteButton->setText("Delete");
+    ui->todosTables->setCellWidget(rowCount, 3, deleteButton);
 }
